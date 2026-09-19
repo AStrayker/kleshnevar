f626f645f16a4dd078d5339e064e727f99451ec6
AStrayker <astrajker@gmail.com>
2026-09-19 12:50:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-indow.KLESHNEVAR = {
+window.KLESHNEVAR = {
   name: "Kleshnevar",
   tagline: "Раки і креветки. Гарячий бульйон. Доставка.",
   phone: "0636503959",
   phonePretty: "+380 63 650 39 59",
   phoneHref: "tel:+380636503959",
   address: "Адреса скоро з'явиться",
-  mapsQuery: "https://www.instagram.com/kleshnevar_km_/",
-  instagram: "https://instagram.com/",
+  mapsQuery: "Хмельницький",
+  instagram: "https://www.instagram.com/kleshnevar_km_/",
   tiktok: "https://www.tiktok.com/",
   telegram: "https://t.me/",
   workHours: "Щодня 10:00 – 20:00",
