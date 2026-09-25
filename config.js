2c2c09873ffc81968a3808fbd72679c5354f6a17
AStrayker <astrajker@gmail.com>
2026-09-25 13:55:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 window.KLESHNEVAR = {
   name: "Kleshnevar",
   tagline: "Раки і креветки. Гарячий бульйон. Доставка.",
-  phone: "0636503959",
+  phone: "380636503959",
   phonePretty: "+380 63 650 39 59",
   phoneHref: "tel:+380636503959",
-  address: "Адреса скоро з'явиться",
-  mapsQuery: "Хмельницький",
+  address: "просп. Шевченко, 31/32",
+  mapsQuery: "просп. Шевченко, 31/32, Каменское, Днепропетровская область, Украина, 51909",
   instagram: "https://www.instagram.com/kleshnevar_km_/",
   tiktok: "https://www.tiktok.com/",
   telegram: "https://t.me/",
-  workHours: "Щодня 10:00 – 20:00",
+  workHours: "будні 10-21, у вихідні 11-22",
   minOrder: 999,
   deliveryNote:
-    "Доставка по місту 60–90 хв згідно з тарифами перевізника.",
+    "Доставка згідно з тарифами перевізника.",
 };
